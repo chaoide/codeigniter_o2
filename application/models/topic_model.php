8b09c9d465b6a6d9fe6273e64e7713eee6d33812
chaoide <geunkilchoi@gmail.com>
2017-12-19 19:36:01 +0900

    Helper 예제 추가

--- application/models/topic_model.php
+++ application/models/topic_model.php
@@ -10,6 +10,10 @@ class Topic_model extends CI_Model {
 	}
 
 	public function get($topic_id) {
+		$this->db->select('id');
+		$this->db->select('title');
+		$this->db->select('description');
+		$this->db->select('UNIX_TIMESTAMP(created) AS created');
 		return $this->db->get_where('topic', array('id'=>$topic_id))->row();
 	}
 }
--- application/views/get.php
+++ application/views/get.php
@@ -2,6 +2,7 @@
 	<article>
 		<h1><?=$topic->title?></h1>
 		<div>
+			<div><?=kdate($topic->created)?></div>
 			<?=$topic->description?>
 		</div>
 	</article>
