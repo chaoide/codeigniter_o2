68686fbd62fe0a6689d7f4b94d0ec97d9b1af355
chaoide <geunkilchoi@gmail.com>
2017-12-19 13:14:38 +0900

    Model 예제 추가

--- application/controllers/topic.php
+++ application/controllers/topic.php
@@ -3,9 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Topic extends CI_Controller {
 
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+		$this->load->model('topic_model');
+	}
+
 	public function index()
 	{
 		$this->load->view('head');
+		$topics = $this->topic_model->gets();
+		$this->load->view('topic_list', array('topics'=>$topics));
 		$this->load->view('main');
 		$this->load->view('footer');
 	}
@@ -13,7 +22,10 @@ class Topic extends CI_Controller {
 	public function get($id)
 	{
 		$this->load->view('head');
-		$this->load->view('get', array('id'=>$id));
+		$topics = $this->topic_model->gets();
+		$this->load->view('topic_list', array('topics'=>$topics));
+		$topic = $this->topic_model->get($id);
+		$this->load->view('get', array('topic'=>$topic));
 		$this->load->view('footer');
 	}
 }
