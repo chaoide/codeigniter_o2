7895dbd565ab2b2f0b99441ff874d84cf7aa2187
chaoide <geunkilchoi@gmail.com>
2017-12-21 10:49:37 +0900

    CLI 예제 추가

---
+++ application/controllers/cli/batch.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Batch extends MY_Controller {
+
+	function __construct() {
+		parent::__construct();
+	}
+
+	function process() {
+		$this->load->model('user_model');
+		$users = $this->user_model->gets();
+
+		$this->load->library('email');
+		$this->email->initialize(array('mailtype'=>'html'));
+
+		foreach($users as $user) {
+			$this->email->from('egoing@gmail.com', 'egoing');
+			$this->email->to($user->email);
+			$this->email->subject('test');
+			$this->email->message('test');
+			$this->email->send();
+			echo "{$user->email}로 메일 전송을 성공 했습니다.\n";
+		}
+	}
+}
