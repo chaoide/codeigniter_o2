d065748632d28bb015e81615441d02a1172ef739
chaoide <geunkilchoi@gmail.com>
2017-12-21 09:03:36 +0900

    메일 전송 기능과 라이브러리 확장

--- application/controllers/topic.php
+++ application/controllers/topic.php
@@ -57,7 +57,24 @@ class Topic extends MY_Controller {
 		else
 		{
 			$topic_id = $this->topic_model->add($this->input->post('title'), $this->input->post('description'));
+
+			// 메일 전송
+			$this->load->model('user_model');
+			$users = $this->user_model->gets();
+
+			$this->load->library('email');
+			$this->email->initialize(array('mailtype'=>'html'));
+
 			$this->load->helper('url');
+
+			foreach($users as $user) {
+				$this->email->from('egoing@gmail.com', 'egoing');
+				$this->email->to($user->email);
+				$this->email->subject('새로운 글이 등록 되었습니다.');
+				$this->email->message('<a href="'.site_url('/topic/get/'.$topic_id).'">'.$this->input->post('title').'</a>');
+				$this->email->send();
+			}
+
 			redirect('/topic/get/'.$topic_id);
 		}
 
